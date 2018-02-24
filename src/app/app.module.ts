14291934df54fc1535225f19fa43f6fff3e81936
Aatish <aatishrana8@gmail.com>
2018-02-24 12:35:26 +0530

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,42 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {NavComponent} from './nav/nav.component';
+import {RouterModule} from '@angular/router';
+import {DashboardComponent} from './home/dashboard/dashboard.component';
+import {PlanComponent} from './home/plan/plan.component';
+import {WorkComponent} from './home/work/work.component';
+import {TeamComponent} from './home/team/team.component';
+import {ProjectComponent} from './home/project/project.component';
+import {ProductComponent} from './home/product/product.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    DashboardComponent,
+    PlanComponent,
+    WorkComponent,
+    TeamComponent,
+    ProjectComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent,
+        children: [
+          {path: '', component: DashboardComponent, pathMatch: 'full'},
+          {path: 'plan', component: PlanComponent},
+          {path: 'work', component: WorkComponent},
+          {path: 'team', component: TeamComponent},
+          {path: 'project', component: ProjectComponent},
+          {path: 'product', component: ProductComponent},
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
